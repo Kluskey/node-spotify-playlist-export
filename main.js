76d0188e323beecdf71d1076647f2f6973510c76
Jared McCluskey <jared.daniel.mccluskey@gmail.com>
2018-07-28 23:45:24 -0600

    Adding write to file shell

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
-var SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node');
+const Promise = require('bluebird');
 
 var spotifyApi = new SpotifyWebApi({
     clientId: process.env.clientId,
     clientSecret: process.env.clientSecret
 });
+
+var promiseArray = [];
+var tracksArray = [];
+var totalTracks = 0;
+var username = "kluskey";
+var playlistId = "5r9BATxFQONQs6EUdXncAX";
 
 // Retrieve an access token
 spotifyApi.clientCredentialsGrant().then(
@@ -26,15 +33,85 @@ spotifyApi.clientCredentialsGrant().then(
 );
 
 function runBackup() {
+    getTotalTracks()
+        .then(function(totalTracks) {
+            console.log('total:', totalTracks);
 
-    spotifyApi.getPlaylist('kluskey', '5r9BATxFQONQs6EUdXncAX')
-        .then(function(data) {
-            // console.log('Some information about this playlist', data.body);
-            let totalTracks = data.body.tracks.total;
+            //we can only pull 100 at a time
+            // for (let index = 0; index < totalTracks; index += 100) {
+            //     console.log('Pulling batch:', index)
+            //     promiseArray.push(pullBatch(index))
+            // }
+            pullBatch(0);
 
-            // console.log(items);
+            return Promise.all(promiseArray)
+        })
+        .then(function(result) {
+            writeExportsToFile();
+        })
+        .catch(function(error) {
+            console.error(error);
+        })
+}
 
-        }, function(err) {
-            console.log('Something went wrong!', err);
-        });
+function pullBatch(startIndex) {
+    return new Promise(function(resolve, reject) {
+
+        spotifyApi.getPlaylistTracks(username, playlistId, {
+                offset: startIndex,
+                limit: 100
+            })
+            .then(function(returnData) {
+                // console.log('Some information about this playlist', data.body);
+                let data = returnData.body;
+                // console.log(JSON.stringify(data, null, 2));
+
+                data.items.forEach(item => {
+                    // console.log(item.track.artists);
+                    let artistNames = [];
+                    item.track.artists.forEach(artist => {
+                        // console.log(element.name);
+                        artistNames.push(artist.name)
+                    });
+                    addTrackToExport(item.track.name, artistNames.join(", "), item.track.album.name, item.track.external_urls.spotify);
+                });
+
+
+            }, function(err) {
+                console.log('Something went wrong!', err);
+                reject(err);
+            });
+
+    });
+}
+
+function getTotalTracks() {
+    return new Promise(function(resolve, reject) {
+
+        spotifyApi.getPlaylistTracks(username, playlistId)
+            .then(function(returnData) {
+                let data = returnData.body;
+
+                //update the total of all tracks
+                totalTracks = data.total;
+
+                resolve(totalTracks);
+
+            }, function(err) {
+                console.log('Something went wrong!', err);
+                reject(err);
+            });
+
+    });
+}
+
+function addTrackToExport(trackName, artistName, albumName, url) {
+    let trackString = trackName + " | " + artistName + " | " + albumName + " | " + url;
+    console.log(trackString);
+
+}
+
+function writeExportsToFile() {
+    console.log("Writing tracks to file... Please wait.");
+
 }
